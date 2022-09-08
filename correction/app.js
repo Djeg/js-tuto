30cffe16139f847194a0f356806eaf9cc5d95c6e
David Jegat <david.jegat@gmail.com>
2022-09-08 16:07:21 +0200

    Correction : "Suppression d'un todo"

--- correction/app.js
+++ correction/app.js
@@ -1,4 +1,16 @@
 /**
+ * Cette fonction supprime une tache de la liste
+ */
+function deleteTodo(li) {
+  return function () {
+    // je vais séléctioner la list des todos
+    const ul = document.querySelector(".todo-list");
+    // On enléve de la list la balise li donnée en paramètre
+    ul.removeChild(li);
+  };
+}
+
+/**
  * Cette fonction est lancé lors du clique sur le bouton
  * ajouter.
  */
@@ -15,6 +27,8 @@ function addTodo() {
   const deleteButton = document.createElement("button");
   // On ajoute du text dans le bouton
   deleteButton.innerText = "Supprimer";
+  // on attache un événement click sur notre bouton de suppression
+  deleteButton.addEventListener("click", deleteTodo(li));
 
   // On ajout le bouton dans la balise li
   li.appendChild(deleteButton);
