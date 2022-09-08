2813dc9e6b412902292843d8cdec7fa2536e0fb2
David Jegat <david.jegat@gmail.com>
2022-09-08 15:58:15 +0200

    Correction : "Ajout de la tache"

--- correction/app.js
+++ correction/app.js
@@ -1,8 +1,23 @@
+/**
+ * Cette fonction est lancé lors du clique sur le bouton
+ * ajouter.
+ */
 function addTodo() {
+  // On séléctionne l'input dans notre fichier HTML
   const input = document.querySelector(".todo-input");
 
-  console.log(input.value);
+  // création d'une balise html LI
+  const li = document.createElement("li");
+  // Mets le contenue de l'input dans le text de la balise
+  li.innerText = input.value;
+
+  // Récupération de la list "ul" des todo
+  const ul = document.querySelector(".todo-list");
+  // Ajouter le li dans la balise ul
+  ul.appendChild(li);
 }
 
+// On séléctionne le bouton "Ajouter"
 const button = document.querySelector(".todo-btn");
+// On attache un événement click sur le bouton
 button.addEventListener("click", addTodo);
