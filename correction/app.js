786fedb625e055f058d41c18dba3269da3fa655f
David Jegat <david.jegat@gmail.com>
2022-09-08 15:50:01 +0200

    Correction : "La valur de l'input"

--- correction/app.js
+++ correction/app.js
@@ -1,8 +1,8 @@
 function addTodo() {
-  console.log("addTodo");
+  const input = document.querySelector(".todo-input");
+
+  console.log(input.value);
 }
 
-const additionner = (x, y) => x + y;
-
 const button = document.querySelector(".todo-btn");
-button.addEventListener("click", additionner);
+button.addEventListener("click", addTodo);
