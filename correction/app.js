a35a9fbc8be4f3cfbcf8632cd8e629e1e020b2da
David Jegat <david.jegat@gmail.com>
2022-09-08 15:47:30 +0200

    Correction : "Click sur le bouton ajouter"

---
+++ correction/app.js
@@ -0,0 +1,8 @@
+function addTodo() {
+  console.log("addTodo");
+}
+
+const additionner = (x, y) => x + y;
+
+const button = document.querySelector(".todo-btn");
+button.addEventListener("click", additionner);
